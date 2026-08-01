a9374845ad11af9baac57e06de9dc99e9a72332b
unknown <cdn5616@nyu.edu>
2026-08-01 17:21:29 -0400

    change picture

--- javascript/script.js
+++ javascript/script.js
@@ -21,10 +21,3 @@ window.addEventListener('scroll', () => {
     }
     lastScrolY = currentScrollY;
 });
-const hamMenu = document.querySelector('.ham-menu');
-const offScreenMenu = document.querySelector('.off-screen-menu');
-
-hamMenu.addEventListener('click', () => {
-    offScreenMenu.classList.toggle('active');
-    document.body.classList.toggle('no-scroll');
-});
